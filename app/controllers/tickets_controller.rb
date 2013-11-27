8a2e4775d734cfa5d178c9e2943d0053db2690aa
Phil Worthington <pworthington27@gmail.com>
2013-11-26 22:50:52 -0800

    Build controller and db

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,2 +1,37 @@
 class TicketsController < ApplicationController
+  def index
+    @tickets = Ticket.all
+  end
+
+  def new
+    @ticket = Ticket.new
+  end
+
+  def create
+    @ticket = Ticket.new(ticket_params)
+
+    if @ticket.save
+      TicketMailer.new_ticket(@ticket).deliver
+      redirect_to @ticket
+    else
+    end
+  end
+
+  def show
+    @ticket = Ticket.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+    def ticket_params
+      params.require(:ticket).permit(:name, :email, :issue)
+    end
 end
